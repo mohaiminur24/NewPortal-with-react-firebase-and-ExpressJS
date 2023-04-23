d4c153cd685d347e4be367d8d31daccd7c00d6cd
DeveloperMMR <mohaiminur24@gmail.com>
2023-04-23 17:14:59 +0600

    header complete

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,26 @@
 import { useContext } from "react";
 import { userContext } from "./AuthContextLayout/AuthContext";
+import moment from "moment/moment";
 
 const Header = () => {
-    const data = useContext(userContext);
-    console.log(data);
+
+    const {user} = useContext(userContext);
+    const date = moment().format('LLLL');;
+
     return (
-        <div>
+        <div className="w-11/12 mx-auto text-center my-5">
+            <img className="w-1/4 mx-auto" src="logo.png" alt="" />
+            <h3 className="text-sm text-gray-400 mt-3">journalism without Fear and Favour</h3>
+            <h2 className="text-sm text-gray-400 mb-3">{date}</h2>
+            <div className="w-full h-auto bg-gray-200 py-2 px-2 flex items-center rounded-sm my-5">
+                <button className="px-3 py-2 font-semibold text-white space-x-16 rounded-sm bg-red-500 mr-auto">Latest</button>
+                <div className="w-11/12 mx-auto">
+                <marquee className="font-semibold">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus officia provident eius eum quibusdam recusandae saepe illo ratione cupiditate earum!
+                </marquee>
+                </div>
+
+            </div>
             
             
         </div>
